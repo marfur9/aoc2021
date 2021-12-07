8c044887196b6c3bbc7b32004b1d8040977a3f32
martinfure <mf@dataloy.com>
2021-12-07 15:31:32 +0100

    stuff

--- day4/day4.py
+++ day4/day4.py
@@ -1,1 +1,14 @@
-inputfile = open('day4/day4.txt', 'r')
+inputfile = open('day4/day4sample.txt', 'r')
+
+
+
+
+
+for inp in inputfile:
+    fromvalue = inp.split()[0]
+    tovalue = inp.split()[2]
+    fromx = fromvalue.split(',')[0]
+    fromy = fromvalue.split(',')[1]
+    tox = tovalue.split(',')[0]
+    toy = tovalue.split(',')[1]
+    
