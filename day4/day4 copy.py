a65b29cd10220691b322936e5630bdd0659d00b9
martinfure <mf@dataloy.com>
2021-12-08 16:22:38 +0100

    progress?=

---
+++ day4/day4 copy.py
@@ -0,0 +1,84 @@
+inputfile = open('day4/day4sample.txt', 'r')
+
+
+paths=[]
+answer=0
+
+for inp in inputfile:
+    fromvalue = inp.split()[0]
+    tovalue = inp.split()[2]
+    fromx = fromvalue.split(',')[0]
+    fromy = fromvalue.split(',')[1]
+    tox = tovalue.split(',')[0]
+    toy = tovalue.split(',')[1]
+
+    path = []
+    ylength=0
+    xlength=0
+    pathlength=0
+    xascending=False
+    yascending=False
+    ystill=False
+    xstill=False
+    if fromy > toy:
+        ylength=int(fromy)-int(toy)
+    elif fromy < toy:
+        ylength=int(toy)-int(fromy)
+        yascending=True
+    elif fromy==toy:
+        ystill=True
+    if fromx > tox:
+        xlength=int(fromx)-int(tox)
+    elif fromx < tox:
+        xascending=True
+        xlength=int(tox)-int(fromx)
+    elif fromx==tox:
+        xstill=True
+    if xlength>ylength:
+        pathlength=xlength
+    elif xlength<=ylength:
+        pathlength=ylength
+    
+    running=True
+    i=1
+    currentvalue=fromvalue
+    currentx=int(fromx)
+    currenty=int(fromy)
+    path.append(currentvalue)
+    while(running):
+        if xascending and not xstill:
+            currentx+=1
+        elif not xascending and not xstill:
+            currentx-=1
+        if yascending and not ystill:
+            currenty+=1
+        elif not yascending and not ystill:
+            currenty-=1
+        
+        currentvalue=str(currentx)+","+str(currenty)
+        path.append(currentvalue)
+        i+=1
+        if i>pathlength:
+            running=False
+    paths.append(path)
+
+
+print(paths)
+
+positions=["0,0-0"]
+for path in paths:
+    for position in path:
+        found=False
+        for i, visitedpos in enumerate(positions):
+            visitedposPosition=visitedpos.split("-")[0]
+            visitedposvalue=int(visitedpos.split("-")[1])
+            if visitedpos.split("-")[0] == position:
+                found=True
+                visitedposvalue = visitedposvalue + 1
+                positions[i]=visitedposPosition+"-"+str(visitedposvalue)
+                if visitedposvalue == 2:
+                    answer+=1
+        if not found:
+            positions.append(position+"-1")
+print(positions)
+print(answer)
--- day4/day4.py
+++ day4/day4.py
@@ -1,8 +1,8 @@
-inputfile = open('day4/day4sample.txt', 'r')
+inputfile = open('day4/day4.txt', 'r')
 
 
 paths=[]
-
+answer=0
 
 for inp in inputfile:
     fromvalue = inp.split()[0]
@@ -20,54 +20,65 @@ for inp in inputfile:
     yascending=False
     ystill=False
     xstill=False
-    if fromy > toy:
-        ylength=int(fromy)-int(toy)
-    elif fromy < toy:
-        ylength=int(toy)-int(fromy)
-        yascending=True
-    elif fromy==toy:
-        ystill=True
-    if fromx > tox:
-        xlength=int(fromx)-int(tox)
-    elif fromx < tox:
-        xascending=True
-        xlength=int(tox)-int(fromx)
-    elif fromx==tox:
-        xstill=True
-    if xlength>ylength:
-        pathlength=xlength
-    elif xlength<=ylength:
-        pathlength=ylength
+    if fromy==toy or fromx==tox:
+        if fromy > toy:
+           ylength=int(fromy)-int(toy)
+        elif fromy < toy:
+            ylength=int(toy)-int(fromy)
+            yascending=True
+        elif fromy==toy:
+            ystill=True
+        if fromx > tox:
+            xlength=int(fromx)-int(tox)
+        elif fromx < tox:
+            xascending=True
+            xlength=int(tox)-int(fromx)
+        elif fromx==tox:
+            xstill=True
+        if xlength>ylength:
+            pathlength=xlength
+        elif xlength<=ylength:
+            pathlength=ylength
     
-    running=True
-    i=1
-    currentvalue=fromvalue
-    print("")
-    print("")
-    currentx=int(fromx)
-    currenty=int(fromy)
-    path.append(currentvalue)
-    while(running):
-        if xascending and not xstill:
-            currentx+=1
-        elif not xascending and not xstill:
-            currentx-=1
-        if yascending and not ystill:
-            currenty+=1
-        elif not yascending and not ystill:
-            currenty-=1
-        
-        currentvalue=str(currentx)+","+str(currenty)
+        running=True
+        i=1
+        currentvalue=fromvalue
+        currentx=int(fromx)
+        currenty=int(fromy)
         path.append(currentvalue)
-        i+=1
-        if i>pathlength:
-            running=False
-    paths.append(path)
+        while(running):
+            if xascending and not xstill:
+                currentx+=1
+            elif not xascending and not xstill:
+                currentx-=1
+            if yascending and not ystill:
+                currenty+=1
+            elif not yascending and not ystill:
+                currenty-=1
+        
+            currentvalue=str(currentx)+","+str(currenty)
+            path.append(currentvalue)
+            i+=1
+            if i>pathlength:
+                running=False
+        paths.append(path)
 
+print("part 1 done")
 
-print(len(paths))
-
-
+positions=["0,0-0"]
 for path in paths:
-    print(path)
-
+    for position in path:
+        found=False
+        for i, visitedpos in enumerate(positions):
+            visitedposPosition=visitedpos.split("-")[0]
+            visitedposvalue=int(visitedpos.split("-")[1])
+            if visitedpos.split("-")[0] == position:
+                found=True
+                visitedposvalue = visitedposvalue + 1
+                positions[i]=visitedposPosition+"-"+str(visitedposvalue)
+                if visitedposvalue == 2:
+                    answer+=1
+        if not found:
+            positions.append(position+"-1")
+print(answer)
+print("part 2 done")
